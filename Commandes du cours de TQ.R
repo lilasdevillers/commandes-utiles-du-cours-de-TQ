acb0d6809e1149ac88a492ffc45f6d333d3b0d51
lilas <81485209+lilasdevillers@users.noreply.github.com>
2021-10-08 14:28:02 +0200

    Update Commandes du cours de TQ.R

--- Commandes du cours de TQ.R
+++ Commandes du cours de TQ.R
@@ -39,7 +39,7 @@ EE20 <- EE20 %>% mutate(across(where(is_character), as.factor)) #changement du m
 wtd.mean(EE20$ACTOP_01, weights = EE20$EXTRIAN)
 
 ## utilisation du package survey
-test_bw <- svydesign(ids = ~1, data = EE20, weights = ~EE20$EXTRIAN) # configurer l'outil survey
+test_bw <- svydesign(ids = ~1, data = EE20, weights = ~EE20$EXTRIAN) # configurer l'outil survey sous le nom de test_bw (cela correspond à bw chez la prof)
 svymean(~ACTOP_01, na.rm = TRUE, test_bw) # donne 
 svymean(~ACTOP_01, na.rm = FALSE, test_bw)
 ## /!\ ajouter na.rm = TRUE pour enlever les NA et na.rm = FALSE pour les garder (en fait non ca ne marche pas de les garder, demander à la  prof pour la pertinence)
@@ -52,12 +52,12 @@ confint(svymean(~ACTOP_01, na.rm = TRUE, test_bw), level=0.99) # au seuil de 99%
 confint(svymean(~ACTOP_01, na.rm = TRUE, test_bw), level=0.9) # au seuil de 90%
 
 ## tests statistiques
-res <- svymean(~ACTOP_01, na.rm = TRUE, test_bw)
-f.actop <- res[1]
-SE.f.actop <- SE(res)
-stat.student.actop <- (f.actop - 0.402 )/SE.f.actop
-df <-length(EE20$ACTOP_01)-1
-pval <- 2*(1-pt(abs(stat.student.actop),df))
+res <- svymean(~ACTOP_01, na.rm = TRUE, test_bw) # sauvegarde du résultat de la commande svymean sous le nom de "res"
+f.actop <- res[1] # le résultat de la première colonne de svymean (qui donne un tableau) est la fréquence et par cette commande on l'extrait et le nomme "f.actop"
+SE.f.actop <- SE(res) # ici on fait qqchose de similaire en extrayant le résultat de la 2e colonne ie la "standard error" SE
+stat.student.actop <- (f.actop - 0.402 )/SE.f.actop # calcul de la statistique de test pour ACTOP
+df <-length(EE20$ACTOP_01)-1 # calcul du nmbre de d° de liberté
+pval <- 2*(1-pt(abs(stat.student.actop),df)) # calcul de la p-value
 
 # Si test d'hypo avec H0 = x et H1 : p =/= x alors on utilise la commande suivante: 
 pval <- 2*(1-pt(abs(stat.student.actop),df))
